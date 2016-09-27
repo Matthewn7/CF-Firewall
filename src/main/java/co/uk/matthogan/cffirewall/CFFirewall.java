a85102024a075b348caa3fc614dd41476c7d30c1
matt <matt@matthogan.co.uk>
2016-09-27 15:36:30 -0700

    Initial commit

---
+++ src/main/java/co/uk/matthogan/cffirewall/CFFirewall.java
@@ -0,0 +1,15 @@
+package co.uk.matthogan.cffirewall;
+
+/**
+ * @author Matthew Hogan
+ */
+public class CFFirewall {
+
+    /**
+     * @param args CLI args
+     */
+    public static void main(String... args) {
+
+        System.out.println("Do I compile?");
+    }
+}
