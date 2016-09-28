03ffbff6feff4b008af9f1b66c76c6e3dbcb02b6
matt <matt@matthogan.co.uk>
2016-09-28 00:03:38 -0700

    Support IPv6

--- src/main/java/co/uk/matthogan/cffirewall/CFFirewall.java
+++ src/main/java/co/uk/matthogan/cffirewall/CFFirewall.java
@@ -16,26 +16,39 @@ import java.util.Scanner;
 public class CFFirewall {
 
     private static final String CF_IPV4_URL;
+    private static final String CF_IPV6_URL;
     private static final String FIREWALL_CMD;
 
     static {
         CF_IPV4_URL = "https://www.cloudflare.com/ips-v4/";
+        CF_IPV6_URL = "https://www.cloudflare.com/ips-v6/";
         FIREWALL_CMD = "iptables -I INPUT -p tcp -m multiport --dports 80,443 -s %s -j ACCEPT -m comment --comment \"CloudFlare IP\"";
     }
 
     /**
-     * @param args CLI args
+     * @param args ipv4 or ipv6
      */
     public static void main(String... args) throws IOException, InterruptedException {
 
+        if (args.length < 1 || args.length > 1 || (!args[0].equals("ipv4") && !args[0].equals("ipv6"))) {
+
+            System.out.println("Please specify either \"ipv4\" or \"ipv6\"");
+            return;
+        }
+
+        boolean ipv4 = args[0].equals("ipv4");
+
         // Download the latest IP ranges from the CloudFlare provided page
-        Scanner scanner = new Scanner(new URL(CF_IPV4_URL).openStream(), "UTF-8").useDelimiter("A");
+        Scanner scanner = new Scanner(new URL(ipv4 ? CF_IPV4_URL : CF_IPV6_URL).openStream(), "UTF-8").useDelimiter("A");
+
         String ipRanges = Jsoup.parse(scanner.next()).body().text();
         scanner.close();
 
         for (String ipRange : ipRanges.split("\\r?\\n")) {
 
-            String command = String.format(Locale.ENGLISH, FIREWALL_CMD, ipRange);
+            String formattedCommand = String.format(Locale.ENGLISH, FIREWALL_CMD, ipRange);
+            String command = (ipv4 ? formattedCommand : formattedCommand.replace("iptables", "ip6tables"));
+
             Runtime.getRuntime().exec(new String[]{"/bin/sh", "-c", command});
 
             // Output the command for debugging
